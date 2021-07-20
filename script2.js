a59f7e1c300fe836286f95200a2bc96fe26fe9a7
sophiiasun <53122458+sophiiasun@users.noreply.github.com>
2021-07-20 12:55:28 -0400

    problem with disable

--- script2.js
+++ script2.js
@@ -7,6 +7,7 @@ var currentPlayer
 
 let gameboard = []
 let data = [[], [], [], [], []]
+let winStatus = [8]
 var slotCounter = 0
 
 main()
@@ -17,7 +18,6 @@ function main() {
 }
 
 function createBoard() {
-    gameboard = []
     for (var r = 0; r < 5; r++) {
         let row = []
         if (r == 0) {
@@ -65,6 +65,7 @@ function createBoard() {
             counter++
         })
     })
+    for (var i = 0; i < 5; i++) data[i] = []
 }
 
 function displayText() {
@@ -81,20 +82,18 @@ function on_click(slot) {
         slotElement.style.backgroundColor = COLOURS[currentPlayer]
         data[R][C] = PLAYERS[currentPlayer]
         slotCounter++
-        currentPlayer = (currentPlayer + 1) % 2
-        var condition = checkWin()
-        if (condition != 'RED' && condition != 'BLUE' && condition != 'TIE') return
-        if (condition == 'RED' || condition == 'BLUE') displayWin(condition)
-        if (condition == 'TIE') displayTie()
-        disableGame()
         setTimeout(function() {
-            wait(2000)
-            resetGame()
+            var condition = checkWin()
+            if (condition == 'RED' || condition == 'BLUE') displayWin(condition)
+            if (condition == 'Tie') displayTie()
+            currentPlayer = (currentPlayer + 1) % 2
         })
+        
     }
 }
 
 function checkWin() {
+    // alert("checking")
     for (var r = 1; r < 4; r++) { // horizontals
         if (data[r][0] == data[r][1] && data[r][1] == data[r][2]) return data[r][0]
     }
@@ -103,18 +102,20 @@ function checkWin() {
     for (var c = 0; c < 3; c++) { // verticals
         if (data[1][c] == data[2][c] && data[2][c] == data[3][c]) return data[1][c]
     }
-    if (slotCounter == 9) return 'TIE'
+    if (slotCounter == 9) return 'Tie'
     return 'NONE'
 }
 
 function displayTie() {
-    gameboard[0][0].slotElement.innerHTML = "TIE!"
+
 }
 
 function displayWin(colour) {
     gameboard[0][0].slotElement.innerHTML = colour + " WINS!"
     if (colour == 'RED') gameboard[4][0].slotElement.innerHTML = ++SCORES[0]
     else gameboard[4][2].slotElement.innerHTML = ++SCORES[1]
+    disableGame()
+    resetGame()
 }
 
 function wait(ms){
@@ -126,10 +127,11 @@ function wait(ms){
  }
 
 function disableGame() {
+    wait(2000)
     for (var r = 1; r < 4; r++) {
         for (var c = 0; c < 3; c++) {
-            var oldSlot = gameboard[r][c].slotElement
-            var newSlot = document.createElement('div')
+            oldSlot = gameboard[r][c].slotElement
+            newSlot = document.createElement('div')
             newSlot.classList.add("slot")
             newSlot.style.backgroundColor = oldSlot.style.backgroundColor
             oldSlot.parentNode.replaceChild(newSlot, oldSlot);
@@ -144,12 +146,12 @@ function removeElements() {
             slot.slotElement.remove()
         })
     })
+    gameboard = []
 }
 
 function resetGame() {
     data = [[], [], [], [], []]
     currentPlayer = (SCORES[0] + SCORES[1]) % 2
-    slotCounter = 0
     removeElements()
     createBoard()
     displayText()
